01f4c74c651549678539656ba5fbe4020cc0829d
bfyxzls <bfyxzls@sina.com>
2018-01-22 21:28:32 +0800

    添加其它包

--- helloworld.java
+++ helloworld.java
@@ -1,13 +1,15 @@
-import lind.IAuto;
 import lind.Auto;
+import lind.IAuto;
 import lind.Student;
 import lind.StudentDemo;
 public class helloworld {
-     public static void main(String[]agrs) {
-       System.out.println("HelloWorld!"); 
+  public static void main(String[] args) {
+    System.out.println("HelloWorld!");
 
-       IAuto auto1 = new Auto(); 
-       auto1.printMsg("123"); 
-       new StudentDemo().print();
-     }
-    }
+    IAuto auto1 = new Auto();
+    auto1.printMsg("123");
+     new StudentDemo().print();
+
+  }
+}
+
--- lind/StudentDemo.java
+++ lind/StudentDemo.java
@@ -6,27 +6,21 @@ package lind;
  */
 public class StudentDemo {
     public  void print() {
-        // 创建对象数组
         Student[] students = new Student[3];
 
-        // 默认值是null
         for (int x = 0; x < students.length; x++) {
             System.out.println(students[x]);
         }
 
-        // 创建学生对象元素
         Student s1 = new Student("zhangsan", 28);
         Student s2 = new Student("lisi", 31);
         Student s3 = new Student("wanger", 18);
 
-        // 把元素放到数组中
         students[0] = s1;
         students[1] = s2;
         students[2] = s3;
 
-        // 赋值后的学生对象
         for (int x = 0; x < students.length; x++) {
-            // System.out.println(students[x]);
             Student s = students[x];
             System.out.println(s.getName() + "---" + s.getAge());
         }
